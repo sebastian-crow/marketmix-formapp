9c0c5fc21efbcc8ff1d5e44c44958c7a3931beee
sebastian-crow <sebastianberiio@gmail.com>
2023-01-24 18:09:59 -0500

    Calculate age ready

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ createApp({
     };
   },
   methods: {
+    getAge(birthdate) {
+      let ageNow = Date.now() - birthdate.getTime();
+      let ageDate = new Date(ageNow);
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
+    },
     randomPass() {
       const randomString = () => {
         let string = Math.random().toString(36).slice(2);
@@ -38,11 +43,12 @@ createApp({
           userName: this.username,
           randomPass: this.randomPass(),
           birthDate: this.birthdate,
+          age: this.getAge(new Date(this.birthdate)),
         });
       }
     },
   },
   mounted() {
-    this.randomPass();  // Working good
+    this.randomPass(); // Working good
   },
 }).mount("#root");
